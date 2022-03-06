8f64367d88b13ca7597034ea65069f5daef0d7dc
Lilia Bedidi <lilia.bedidi@gmail.com>
2022-03-06 19:37:39 +0100

    fix hey sentence

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -88,7 +88,7 @@ class TasksController < ApplicationController
 
   def estimated_time
     t = 0
-    @tasks.not_done.each do |task|
+    @tasks.not_done.today.each do |task|
       t += task.duration unless task.duration.nil?
     end
     return "00:00" if t.zero?
