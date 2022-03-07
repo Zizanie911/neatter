94c651da9c3485855158c96cdc8c5bbd61e2efd1
Stephen Grieves <stephengrieves@gmail.com>
2022-03-07 17:02:26 +0100

    navbar edits

--- app/policies/task_policy.rb
+++ app/policies/task_policy.rb
@@ -14,6 +14,7 @@ class TaskPolicy < ApplicationPolicy
 
   def update?
     record.user == user
+    return true
     # - record: the restaurant passed to the `authorize` method in controller
     # - user:   the `current_user` signed in with Devise.
   end
@@ -24,5 +25,9 @@ class TaskPolicy < ApplicationPolicy
 
   def mark_as_done?
     record.user == user
+  end
+
+  def edit
+    update?
   end
 end
