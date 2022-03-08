194ff907655e2c06d1469f6bef8b798dc8248f24
Lilia Bedidi <lilia.bedidi@gmail.com>
2022-03-08 17:11:28 +0100

    choose postpone date

--- app/controllers/nextday_controller.rb
+++ app/controllers/nextday_controller.rb
@@ -5,6 +5,7 @@ class NextdayController < ApplicationController
   end
 
   def reset
+
     # TODO
     # 1/ Itérer sur les mark_as_done_task_ids de params
 
@@ -21,13 +22,14 @@ class NextdayController < ApplicationController
     # 2/ Itérer sur les postpone_task_ids de params et les cloner avec comme start_date le lendemain
     unless params[:postpone_task_ids].nil?
       @tasks = Task.today.find(params[:postpone_task_ids])
+
       @tasks.each do |task|
         # task.duplicate_to_next_day :
         copy = Task.new(
           name: task.name,
           details: task.details,
           priority: task.priority,
-          start_at: task.start_at + 1.day
+          start_at: params[:postpone_task_date][task.id.to_s]
         )
         copy.user = current_user
         copy.save!
