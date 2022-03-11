ec0a0e4bf9a0c149a52a210a9f3d885730656540
Stephen Grieves <stephengrieves@gmail.com>
2022-03-11 11:46:15 +0100

    ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,8 @@ class PagesController < ApplicationController
   def switch_mode
     current_user.dark_mode = !current_user.dark_mode
     current_user.save
-    redirect_to params[:url]
+    # redirect_to params[:url]
+    head :ok
   end
 
 end
